f96b2f0d348d817d3a0ef6013e8482d3ebb14e2a
macribo <roibeard.mac.unfraidh@gmail.com>
2017-03-29 13:55:56 +0200

    sort() secCode

--- security.js
+++ security.js
@@ -15,5 +15,13 @@ console.log(checksum);
 var secCode = roomData[0].replace(/\-+/g,"");
 
 var secCode = secCode.replace(/\d+/g,"");
+sortCode = secCode.split("");
+sortCode = sortCode.sort();
 console.log(secCode);
 
+
+// puzzle elements:  security-code , sector-id , checksum ,  verified-checksum
+
+
+// use split the security-code and arrange in alphabetical order
+// counting 'like' elements in the array.  find the 5 most frequent elements to verified-checksum (adding elements in alphabetical order when they have the same frequency).  if verified checksup === checksum, add this element's sector-id to sectorIdTotal.
