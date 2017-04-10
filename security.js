d20f732b587089a42058afff7bbc26667edae89d
BOZ2323 <lillibaaske@gmx.de>
2017-04-10 10:54:41 +0200

    Add sort()

--- security.js
+++ security.js
@@ -1,8 +1,8 @@
 //jshint esversion:6
 //var roomData= require("./roomData");
 
-//var roomData = "nzydfxpc-rclop-qwzhpc-qtylyntyr-769[oshgk]";
-var roomData = "aaaaa-bbb-z-y-x-123[abxyz]";
+var roomData = "nzydoofxossspc-rclop-qwzhpc-qtylyntyr-769[oshgk]";
+//var roomData = "aaaaa-bbb-z-y-x-123[abxyz]";
 
 var roomData = roomData.split("[");
 console.log("roomData after split()", roomData);
@@ -22,28 +22,30 @@ console.log("securityCode after replace(","): ",securityCode);
 //var secCode = roomData[0].replace(/\-+/g,"");
 //Trying to use first split(at numbers), and then pop(), to move the last item to a new array. So we keep the ID for later.
 //var secCode = secCode.replace(/\d+/g,"");
-sortCode = securityCode.split("");
-sortCode = sortCode.sort();
+var sortCode = securityCode.split("");
+securityCode = sortCode.sort();
 console.log("sortCode: ",sortCode);
 var checksum = checksum.split("");
 console.log("checksum after split()",checksum);
 
 function arrayCompare(){
+    var codeToCheck = [];
     var count = 0;
-    var arraySecCode = sortCode;
-    var arrayChecksum = checksum;
-    for (var i = 0; i < arraySecCode.length; i++){
-        console.log ("arraySecCode in for loop",arraySecCode[i]);
-        for(var j = 0; j < arrayChecksum.length; j++ ){
-            if(arraySecCode[i] === arrayChecksum[j]){
-                console.log("for loop (j): ", arraySecCode[i]);
+    for (var i = 0; i < securityCode.length; i++){
+        //console.log ("arraySecCode in for loop",arraySecCode[i]);
+        for(var j = 0; j < checksum.length; j++ ){
+            if(securityCode[i] === checksum[j]){
                 count++;
+                console.log("arraySeode[i], count", securityCode[i]+ ":"+ count);
+                codeToCheck.push(securityCode[i]);
             }
         }
     }
+    console.log(codeToCheck);
 }
 arrayCompare();
 // puzzle elements:  security-code , sector-id , checksum ,  verified-checksum
+
 
 
 // use split the security-code and arrange in alphabetical order
